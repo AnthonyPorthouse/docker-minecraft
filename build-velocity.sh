d71ea692fc362c124f7e9bbc338e5a91a3edfc91
Anthony Porthouse <anthony@porthou.se>
2022-05-16 22:25:42 +0100

    Add paper and velocity builds

--- build-velocity.sh
+++ build-velocity.sh
@@ -5,6 +5,9 @@
 # Build Velocity
 versions=$(curl -s 'https://papermc.io/api/v2/projects/velocity' | jq -r ".version_groups[]")
 
+IMAGE_NAME=${IMAGE_NAME:-port3m5/velocity}
+IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
+
 for version in $versions; do
     build=$(curl -s "https://papermc.io/api/v2/projects/velocity/version_group/${version}/builds" | jq .builds[-1])
 
@@ -16,13 +19,13 @@ for version in $versions; do
     docker build \
         --build-arg VELOCITY_VERSION="$version" \
         --build-arg=BUILD_NUMBER="$build_id" \
-        -t "port3m5/velocity" \
-        -t "port3m5/velocity:${version}" \
-        -t "port3m5/velocity:${version}-${build_id}" \
+        -t "$IMAGE_NAME" \
+        -t "$IMAGE_NAME:${version}" \
+        -t "$IMAGE_NAME:${version}-${build_id}" \
         velocity
 
-    docker push "port3m5/velocity:${version}"
-    docker push "port3m5/velocity:${version}-${build_id}"
+    docker push "$IMAGE_NAME:${version}"
+    docker push "$IMAGE_NAME:${version}-${build_id}"
 done;
 
-docker push "port3m5/velocity"
+docker push "$IMAGE_NAME"
