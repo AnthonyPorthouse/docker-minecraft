002f149dcfef3cc88cf216f04086392fa58efde9
Anthony Porthouse <anthony@porthou.se>
2022-05-17 09:44:32 +0100

    Pass version in as environment variable

--- minecraft/build/get-minecraft-versions.sh
+++ minecraft/build/get-minecraft-versions.sh
@@ -5,9 +5,6 @@ if ! command -v jq >/dev/null 2>&1; then
     exit 1
 fi
 
-IMAGE_NAME=${IMAGE_NAME:-port3m5/minecraft}
-IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
-
 manifest=$(curl -s 'https://launchermeta.mojang.com/mc/game/version_manifest.json')
 
 latest_version=$(jq -r '.latest.release' <<< "$manifest")
