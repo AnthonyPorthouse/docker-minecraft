0a130950fab02f00ee19bcc45b19471a981aaceb
Anthony Porthouse <anthony@porthou.se>
2022-05-17 15:08:08 +0100

    Add velocity parallel build

---
+++ velocity/build/build-version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if ! command -v jq >/dev/null 2>&1; then
+    echo "jq is missing" > /dev/stderr
+    exit 1
+fi
+
+latest="${LATEST_VERSION:-}"
+version="${VERSION:-}"
+
+IMAGE_NAME=${IMAGE_NAME:-port3m5/velocity}
+IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
+
+build=$(curl -s "https://papermc.io/api/v2/projects/velocity/version_group/${version}/builds" | jq .builds[-1])
+
+velocity_version=$(jq -r '.version' <<< "$build")
+build_id=$(jq -r '.build' <<< "$build")
+
+echo "Building Velocity $velocity_version build $build_id"
+
+docker build \
+    --build-arg VELOCITY_VERSION="$velocity_version" \
+    --build-arg=BUILD_NUMBER="$build_id" \
+    -t "$IMAGE_NAME" \
+    -t "$IMAGE_NAME:${velocity_version}" \
+    -t "$IMAGE_NAME:${velocity_version}-${build_id}" \
+    paper
+
+docker push "$IMAGE_NAME"
+docker push "$IMAGE_NAME:${velocity_version}"
+docker push "$IMAGE_NAME:${velocity_version}-${build_id}"
---
+++ velocity/build/get-versions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! command -v jq >/dev/null 2>&1; then
+    echo "jq is missing";
+    exit 1
+fi
+
+manifest=$(curl -s 'https://papermc.io/api/v2/projects/velocity')
+
+versions=$(jq -c '.version_groups' <<< "$manifest")
+latest_version=$(jq -r '.version_groups[-1]' <<< "$manifest")
+
+echo "::set-output name=latest::$latest_version"
+echo "::set-output name=versions::$versions"
