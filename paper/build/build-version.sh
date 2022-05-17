3c3c60e89a7db3c0e79137514eb8699722b15262
Anthony Porthouse <anthony@porthou.se>
2022-05-17 10:54:11 +0100

    don't clobber version variable

--- paper/build/build-version.sh
+++ paper/build/build-version.sh
@@ -13,21 +13,21 @@ IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
 
 build=$(curl -s "https://papermc.io/api/v2/projects/paper/version_group/${version}/builds" | jq .builds[-1])
 
-version=$(jq -r '.version' <<< "$build")
+minecraft_version=$(jq -r '.version' <<< "$build")
 build_id=$(jq -r '.build' <<< "$build")
 
-echo "Building Paper $version build $build_id"
+echo "Building Paper $minecraft_version build $build_id"
 
 docker build \
-    --build-arg MINECRAFT_VERSION="$version" \
+    --build-arg MINECRAFT_VERSION="$minecraft_version" \
     --build-arg=BUILD_NUMBER="$build_id" \
     -t "$IMAGE_NAME" \
-    -t "$IMAGE_NAME:${version}" \
-    -t "$IMAGE_NAME:${version}-${build_id}" \
+    -t "$IMAGE_NAME:${minecraft_version}" \
+    -t "$IMAGE_NAME:${minecraft_version}-${build_id}" \
     paper
 
-docker push "$IMAGE_NAME:${version}"
-docker push "$IMAGE_NAME:${version}-${build_id}"
+docker push "$IMAGE_NAME:${minecraft_version}"
+docker push "$IMAGE_NAME:${minecraft_version}-${build_id}"
 
 if [ "$version" == "$latest" ]; then
     docker push "$IMAGE_NAME"
