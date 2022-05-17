7bf47c7c9b0c2d4b5a517b15bf6bf320287df05a
Anthony Porthouse <anthony@porthou.se>
2022-05-17 10:37:47 +0100

    add initial parallel paper builds

---
+++ paper/build/build-version.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if ! command -v jq >/dev/null 2>&1; then
+    echo "jq is missing" > /dev/stderr
+    exit 1
+fi
+
+latest="${LATEST_VERSION:-}"
+version="${VERSION:-}"
+
+IMAGE_NAME=${IMAGE_NAME:-port3m5/minecraft}
+IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
+
+build=$(curl -s "https://papermc.io/api/v2/projects/paper/version_group/${version}/builds" | jq .builds[-1])
+
+version=$(jq -r '.version' <<< "$build")
+build_id=$(jq -r '.build' <<< "$build")
+
+echo "Building Paper $version build $build_id"
+
+docker build \
+    --build-arg MINECRAFT_VERSION="$version" \
+    --build-arg=BUILD_NUMBER="$build_id" \
+    -t "$IMAGE_NAME" \
+    -t "$IMAGE_NAME:${version}" \
+    -t "$IMAGE_NAME:${version}-${build_id}" \
+    paper
+
+docker push "$IMAGE_NAME:${version}"
+docker push "$IMAGE_NAME:${version}-${build_id}"
+
+if [ "$version" == "$latest" ]; then
+    docker push "$IMAGE_NAME"
+fi
