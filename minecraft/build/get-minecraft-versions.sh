b7bfedeac19fb4edf55ed20cc775fba8999d4e6e
Anthony Porthouse <anthony@porthou.se>
2022-05-17 09:14:40 +0100

    Attempt matrix build

---
+++ minecraft/build/get-minecraft-versions.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! command -v jq >/dev/null 2>&1; then
+    echo "jq is missing";
+    exit 1
+fi
+
+IMAGE_NAME=${IMAGE_NAME:-port3m5/minecraft}
+IMAGE_NAME=$(echo "$IMAGE_NAME" | tr '[:upper:]' '[:lower:]')
+
+manifest=$(curl -s 'https://launchermeta.mojang.com/mc/game/version_manifest.json')
+
+latest_version=$(jq -r '.latest.release' <<< "$manifest")
+versions=$(jq '[.versions[] | select( .type == "release" )]' <<< "$manifest")
+
+echo "::set-output name=latest::$latest_version"
+echo "::set-output name=versions::$versions"
